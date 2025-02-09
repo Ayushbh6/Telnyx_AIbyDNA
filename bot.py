d9f747e50ca94272665917b038889ee4a004f7f3
Aparajit Bhattacharya <aparajitbhattacharya@Ayushs-MacBook-Air-2.local>
2025-02-09 15:43:42 +0100

    New 6

--- bot.py
+++ bot.py
@@ -84,8 +84,8 @@ async def run_bot(
     task = PipelineTask(
         pipeline,
         params=PipelineParams(
-            audio_in_sample_rate=8000,
-            audio_out_sample_rate=8000,
+            #audio_in_sample_rate=8000,
+            #audio_out_sample_rate=8000,
             allow_interruptions=True,
         ),
     )
