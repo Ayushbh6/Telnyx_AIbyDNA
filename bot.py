216c2e190ab3f40c39efc95e0c03821174b17e5b
Aparajit Bhattacharya <aparajitbhattacharya@Ayushs-MacBook-Air-2.local>
2025-02-18 12:05:19 +0100

    Deepgram 6

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ async def run_bot(
     llm.register_function("get_company_info", get_company_info, start_callback=start_get_company_info)
 
     stt = DeepgramSTTService(
-            api_key=os.getenv("DEEPGRAM_API_KEY"), live_options=LiveOptions(language="el")
+            api_key=os.getenv("DEEPGRAM_API_KEY"), live_options=LiveOptions(language="el"), model="nova-2"
         )
     
     tts = ElevenLabsTTSService(
